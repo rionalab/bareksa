bdb0cdfc8a8e3fb4a4f08977a311b2c45c707da2
rionalab <rionalab@gmail.com>
2021-10-22 21:59:26 +0700

    add polar chart

--- src/comps/main/section-top/PolarChart.js
+++ src/comps/main/section-top/PolarChart.js
@@ -1,11 +1,73 @@
-import React from 'react'
+import Styles from './style.module.scss';
+import { PolarArea } from 'react-chartjs-2';
+import ChartHeader from './ChartHeader';
 
 function PolarChart() {
+
+    const data = {
+        labels: ['Cat#1', 'Cat#2', 'Cat#3', 'Cat#4'],
+        datasets: [
+            {
+                label: '# of Votes',
+                data: [20, 16, 18, 12],
+                borderWidth: 0,
+                backgroundColor: [
+                    '#EBA45E',
+                    '#E4EAEB',
+                    '#725E9C',
+                    '#5C8F94',
+                ],
+            },
+        ],
+    };
+
+    const options = {
+        responsive: true,
+        segmentShowStroke: false,
+        plugins: {
+            legend: {
+                position: 'bottom',
+                labels: {
+                    boxWidth: 5,
+                    usePointStyle: true,
+                },
+            },
+        },
+        elements: {
+            arc: {
+                borderWidth: 10,
+                borderColor: 'magenta',
+                backgroundColor: 'red',
+            },
+        },
+        scales: {
+            r: {
+                grid: {
+                    display: false,
+                },
+                pointLabels: {
+                    color: 'red',
+                },
+                angleLines: {
+                    color: 'magenta',
+                    lineWidth: 10,
+                },
+                ticks: {
+                    stepSize: 2,
+                    display: false,
+                },
+            },
+        },
+    };
+
     return (
-        <div>
-            polar chart
+        <div className={Styles.container}>
+            <ChartHeader title='Users' type='dotted' />
+            <div className={Styles.chartContainer}>
+                <PolarArea data={data} options={options} />
+            </div>
         </div>
-    )
+    );
 }
 
 export default PolarChart
